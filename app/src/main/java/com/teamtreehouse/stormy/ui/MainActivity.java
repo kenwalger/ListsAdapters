56012a082e6d43609d54685188cd36bcab539e55
kenwalger <kenalger@comcast.net>
2018-04-12 15:53:49 -0700

    Stage 1 Video 6

--- app/src/main/java/com/teamtreehouse/stormy/ui/MainActivity.java
+++ app/src/main/java/com/teamtreehouse/stormy/ui/MainActivity.java
@@ -140,7 +140,28 @@ public class MainActivity extends AppCompatActivity {
     return forecast;
   }
 
-  private Day[] getDailyForecast(String jsonData) {
+  private Day[] getDailyForecast(String jsonData) throws JSONException {
+    JSONObject forecast = new JSONObject(jsonData);
+    String timezone = forecast.getString("timezone");
+
+    JSONObject daily = forecast.getJSONObject("daily");
+    JSONArray data = daily.getJSONArray("data");
+    Day[] days = new Day[data.length()];
+
+    for (int i = 0; i < data.length(); i++ ) {
+      JSONObject jsonDay = data.getJSONObject(i);
+      Day day = new Day();
+
+      day.setSummary(jsonDay.getString("summary"));
+      day.setIcon(jsonDay.getString("icon"));
+      day.setTemperatureMax(jsonDay.getDouble("temperatureMax"));
+      day.setTime(jsonDay.getLong("time"));
+      day.setTimeZone(timezone);
+
+      days[i] = day;
+    }
+
+    return days;
   }
 
   private Hour[] getHourlyForecast(String jsonData) throws JSONException {
@@ -151,6 +172,22 @@ public class MainActivity extends AppCompatActivity {
     JSONObject hourly = forecast.getJSONObject("hourly");
     JSONArray data = hourly.getJSONArray("data");
 
+    Hour[] hours = new Hour[data.length()];
+
+    for (int i = 0; i < data.length(); i++) {
+      JSONObject jsonHour = data.getJSONObject(i);
+      Hour hour = new Hour();
+
+      hour.setSummary(jsonHour.getString("summary"));
+      hour.setIcon(jsonHour.getString("icon"));
+      hour.setTemperature(jsonHour.getDouble("temperature"));
+      hour.setTime(jsonHour.getLong("time"));
+      hour.setTimeZone(timezone);
+
+      hours[i] = hour;
+    }
+
+    return hours;
   }
 
   private Current getCurrentDetails(String jsonData) throws JSONException {
